10c6a80f4d28d4fa2f13b782e5d740aad72c4583
SunnyCodes <smileeesun@snu.ac.kr>
2018-04-30 15:30:45 +0200

    PlaneManager: adds and deletes planes as the character moves forward EnemyManager: throws snowball randomly as the character moves forward

---
+++ UnityKinectDemo/bodyTrackedDemo/Assets/EnemyManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public GameObject[] enemyPrefabs;
+    private Transform characterTransform;
+    private float maxZ;
+    private float gl;
+    private float gns;
+
+    // waiting time
+    private float sleepTime = 100;
+    private float sleepTimeLeft;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterTransform = GameObject.FindGameObjectWithTag("Character").transform;
+        var gm = GameObject.FindGameObjectWithTag("PlaneManager").GetComponent<PlaneManager>();
+        maxZ = gm.spawnZ;
+        gl = gm.groundLength;
+        gns = gm.groundNumScreen;
+        sleepTime = 10000;
+
+        spawnEnemy();
+        sleepTimeLeft = sleepTime;
+        //Debug.Log("maxZ: " + maxZ);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterTransform = GameObject.FindGameObjectWithTag("Character").transform;
+        if (sleepTimeLeft > 0) sleepTimeLeft -= 1.0f;
+        else
+        {
+            spawnEnemy();
+            sleepTimeLeft = sleepTime;
+        }
+    }
+
+    void spawnEnemy()
+    {
+        GameObject go;
+        go = Instantiate(enemyPrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(0, 20), characterTransform.position.z + 10);
+        rb = go.GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), Random.Range(-100, 0)));
+        Debug.Log("snowball position: " + go.transform.position.ToString());
+    }
+}
---
+++ UnityKinectDemo/bodyTrackedDemo/Assets/PlaneManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneManager : MonoBehaviour
+{
+    public GameObject[] groundPrefabs;
+    public float spawnZ;
+    public float groundLength = 20.0f;
+    public int groundNumScreen = 8;
+
+    private Transform characterTransform;
+
+    private List<GameObject> activeGrounds;
+
+    // Use this for initialization
+    void Start()
+    {
+        activeGrounds = new List<GameObject>();
+        characterTransform = GameObject.FindGameObjectWithTag("Character").transform;
+        groundLength = 20;
+        spawnZ = characterTransform.position.z - groundLength / 2;
+        for (int i = 0; i < groundNumScreen; i++)
+        {
+            spawnGround();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterTransform = GameObject.FindGameObjectWithTag("Character").transform;
+        if (spawnZ + groundLength / 2 - characterTransform.position.z < groundNumScreen * groundLength)
+        {
+            spawnGround();
+            deleteGround();
+        }
+    }
+
+    void spawnGround(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(groundPrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += groundLength;
+        activeGrounds.Add(go);
+    }
+
+    void deleteGround()
+    {
+        Debug.Log("DELETE---  position: " + characterTransform.position.z + " / spawnZ: " + spawnZ);
+        Destroy(activeGrounds[0]);
+        activeGrounds.RemoveAt(0);
+    }
+}
