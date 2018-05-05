eecde42c1f6ae4b3084f4757720cbd74c8487118
SunnyCodes <smileeesun@snu.ac.kr>
2018-05-05 19:51:45 +0200

    wall spawn added

--- UnityKinectDemo/bodyTrackedDemo/Assets/Scripts/CoinSpawner.cs
+++ UnityKinectDemo/bodyTrackedDemo/Assets/Scripts/CoinSpawner.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinSpawner : MonoBehaviour {
+public class CoinSpawner : MonoBehaviour
+{
 
     int numCoins = 30;
     float coinSpacing = 1;
@@ -15,23 +16,26 @@ public class CoinSpawner : MonoBehaviour {
     float amplitude = 1;
     float period = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         GameObject coinParent = new GameObject("Coins");
         for (int i = 0; i < numCoins; i++)
         {
             float x = 0;
-            if (sinusoidal) {
-                x=amplitude* Mathf.Sin(period * i);
+            if (sinusoidal)
+            {
+                x = amplitude * Mathf.Sin(period * i);
             }
             GameObject coin = GameObject.Instantiate(Resources.Load("Coin")) as GameObject;
-            coin.transform.position = new Vector3(x+xOffset, yOffset, i * coinSpacing);
+            coin.transform.position = new Vector3(x + xOffset, yOffset, i * coinSpacing);
             coin.transform.parent = coinParent.transform;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
