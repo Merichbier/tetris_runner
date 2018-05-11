04cf858860cd79db6a4074a7cebf5ff86393272d
Alain Birchmeier <alain.birchmeier@outlook.com>
2018-05-11 14:53:09 +0200

    Appearing Walls

--- UnityKinectDemo/bodyTrackedDemo/Assets/Scripts/PlaneManager.cs
+++ UnityKinectDemo/bodyTrackedDemo/Assets/Scripts/PlaneManager.cs
@@ -6,15 +6,15 @@ public class PlaneManager : MonoBehaviour
 {
     public GameObject[] groundPrefabs;
     public float spawnZ = 0;
-    public float groundLength = 20.0f;
+    public float groundLength = 100.0f;
     public int groundNumScreen = 8;
 
     private Transform characterTransform;
 
     private List<GameObject> activeGrounds;
     private int nextToSpawn = 0;
-    private float terrainYOffset = -8.75f;
-    private int THRESHOLD = 5;
+    public static float TERRAIN_Y_OFFSET = -8.75f;
+    public static int THRESHOLD = 5;
 
     // Use this for initialization
     void Start()
@@ -34,7 +34,7 @@ public class PlaneManager : MonoBehaviour
         characterTransform = GameObject.FindGameObjectWithTag("Character").transform;
         if (characterTransform.position.z > spawnZ - (groundNumScreen - 1) * groundLength + THRESHOLD)
         {
-            Debug.Log(characterTransform.position.z + " > " + (spawnZ - groundNumScreen * groundLength + THRESHOLD));
+            //Debug.Log(characterTransform.position.z + " > " + (spawnZ - groundNumScreen * groundLength + THRESHOLD));
             spawnGround();
             deleteGround();
         }
@@ -46,7 +46,7 @@ public class PlaneManager : MonoBehaviour
         go = Instantiate(groundPrefabs[nextToSpawn]) as GameObject;
         nextToSpawn = (nextToSpawn + 1) % groundPrefabs.Length;
         go.transform.SetParent(transform);
-        go.transform.position = new Vector3(-groundLength / 2, terrainYOffset, spawnZ);//characterTransform.forward * spawnZ;
+        go.transform.position = new Vector3(-groundLength / 2, TERRAIN_Y_OFFSET, spawnZ);//characterTransform.forward * spawnZ;
         spawnZ += groundLength;
         activeGrounds.Add(go);
     }
